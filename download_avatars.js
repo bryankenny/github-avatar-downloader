90e40f088c5b6187e0c24aa6b2c2aa86f89f7d4b
Bryan Kenny <b.93.kenny@gmail.com>
2018-10-31 07:24:09 +0000

    step4

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,15 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+ function getRepoContributors(repoOwner, repoName, cb) {
+   var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
