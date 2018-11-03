c5099ce9a1c841d0d001c260ac1a58dfa7a6f816
Bryan Kenny <b.93.kenny@gmail.com>
2018-11-03 23:17:15 +0000

    got authorization token working and have secrets.js being git ignored

--- download_avatars.js
+++ download_avatars.js
@@ -1,18 +1,25 @@
 var request = require('request');
+var token = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
- function getRepoContributors(repoOwner, repoName, cb) {
-   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'a2bc378f06ff457da65f221ab57ee0068b041bac'
+      'Authorization': token
     }
+  };
+
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-}
